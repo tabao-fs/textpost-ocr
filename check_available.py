32ccaea86512f8d48731a3f5d4142c17daf620a5
Tom <abaotom14@gmail.com>
2020-11-03 09:15:32 +0800

    add check available tools and langs

---
+++ check_available.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import sys
+
+import pyocr
+import pyocr.builders
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+# The tools are returned in the recommended order of usage
+tool = tools[0]
+print("Will use tool '%s'" % (tool.get_name()))
+# Ex: Will use tool 'libtesseract'
+
+langs = tool.get_available_languages()
+print("Available languages: %s" % ", ".join(langs))
+lang = langs[0]
+print("Will use lang '%s'" % (lang))
