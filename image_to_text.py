c5ab4b5a0303570ce59e8e10fd6dc5929b4a6bda
Tom <abaotom14@gmail.com>
2020-11-04 08:22:11 +0800

    add builder arg

--- image_to_text.py
+++ image_to_text.py
@@ -4,11 +4,11 @@ import pyocr.builders
 from check_available import get_available_tool, get_available_language
 
 
-def image_to_string(tool, lang, filename):
+def image_to_string(tool, lang, builder, filename):
     return tool.image_to_string(
         Image.open(filename),
         lang=lang,
-        builder=pyocr.builders.TextBuilder()
+        builder=builder
     )
 
 
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     '''
     tool = get_available_tool()
     lang = get_available_language(tool)
+    builder = pyocr.builders.TextBuilder()
     filename = 'data/1.jpg'
 
-    txt = image_to_string(tool, lang, filename)
+    txt = image_to_string(tool, lang, builder, filename)
     print(txt)
