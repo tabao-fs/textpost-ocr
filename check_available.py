bc9f3459623ab3f5f5d2466e48019b6913b96441
Tom <abaotom14@gmail.com>
2020-11-04 08:06:16 +0800

    add get_available_tool

--- check_available.py
+++ check_available.py
@@ -4,16 +4,19 @@ import sys
 import pyocr
 import pyocr.builders
 
-tools = pyocr.get_available_tools()
-if len(tools) == 0:
-    print("No OCR tool found")
-    sys.exit(1)
-# The tools are returned in the recommended order of usage
-tool = tools[0]
-print("Will use tool '%s'" % (tool.get_name()))
-# Ex: Will use tool 'libtesseract'
+
+def get_available_tool():
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        print("No OCR tool found")
+        sys.exit(1)
+
+    return tools[0]
+
+tool = get_available_tool()
+print("Will use tool: '%s'" % (tool.get_name()))
 
 langs = tool.get_available_languages()
 print("Available languages: %s" % ", ".join(langs))
 lang = langs[0]
-print("Will use lang '%s'" % (lang))
+print("Will use lang: '%s'" % (lang))
