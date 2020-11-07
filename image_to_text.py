df06c659c4242b27e62a6a0a2ed3969defcb78a2
Tom <abaotom14@gmail.com>
2020-11-07 08:30:26 +0800

    add convert_image

--- image_to_text.py
+++ image_to_text.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image
 import codecs
 
@@ -5,6 +6,7 @@ from check_available import (
     get_available_tool, get_available_language,
     get_text_builder
 )
+
 
 def image_to_string(tool, lang, builder, filename):
     return tool.image_to_string(
@@ -19,18 +21,20 @@ def write_text_file(filename, text, builder):
         builder.write_file(file_descriptor, text)
 
 
+def convert_image(filename):
+    tool = get_available_tool()
+    lang = get_available_language(tool)
+    builder = get_text_builder()
+    text = image_to_string(tool, lang, builder, filename)
+
+    return text
+
 if __name__ == '__main__':
     '''
     Convert image to text
     '''
-    tool = get_available_tool()
-    lang = get_available_language(tool)
-    builder = get_text_builder()
-    filename = 'data/1.jpg'
-
-    txt = image_to_string(tool, lang, builder, filename)
-
-    filename = 'output/1.txt'
-    write_text_file(filename, txt, builder)
-
-    print(txt)
+    if len(sys.argv) <= 1:
+        print("Image file was not provided. Please try again.")
+    else:
+        text = convert_image(sys.argv[1])
+        print(text)
